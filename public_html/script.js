442e05a64f31119f1089a84dbffbe9687bc33174
andreabesso <andreabexyz@gmail.com>
2021-04-17 17:57:15 +0200

    correzione ora login

--- public_html/script.js
+++ public_html/script.js
@@ -7,7 +7,7 @@
 function init() {
     document.querySelector("#blogin").addEventListener("click", welcome);
     msg = "";
-
+    document.querySelector("#btncol").addEventListener("click", cambiaColore);
 
 }
 
@@ -15,26 +15,30 @@ function welcome() {
     let c = document.querySelector("#cognome");
     let n = document.querySelector("#nome");
     let msg;
-    let nuovadata;
-
+    let tx;
     nuovadata = Date();
-    if (c.value == "" || n.value == "") {
-        msg = "Errore, campi non compilati."
-    }
     if (c.value != "Besso" || n.value != "Andrea") {
-        msg = "Errore, account non registrato."
+        if (c.value == "" || n.value == "") {
+            msg = "Campi non compilati."
+            tx = "Errore!"
+        } else {
+            msg = "Account non registrato."
+            tx = "Errore!"
+        }
     } else {
-        msg = "Benvenuto " + c.value + " " + n.value + "<br>" + "Data e ora accesso: <br>"
-                + nuovadata;
+        msg = "Orario Login: <br>" + convertDate(nuovadata);
+        tx = "Benvenuto " + c.value + " " + n.value;
     }
     document.querySelector("#msg").innerHTML = msg;
+    document.querySelector("#title").innerHTML = tx;
+
 }
 function lampadina(l) {
     var pic;
     if (l == 0) {
-        pic = "img/pic_bulboff.gif";
+        pic = "pic_bulboff.gif";
     } else {
-        pic = "img/pic_bulbon.gif"
+        pic = "pic_bulbon.gif"
     }
     document.querySelector("#lamp").src = pic;
 }
@@ -43,11 +47,20 @@ function visualizza(id) {
     let n = document.querySelector("#nome");
     if (document.getElementById) {
         if (document.getElementById(id).style.display == 'none') {
-            if (c.value == "Besso" || n.value == "Andrea") {
+            if (c.value == "Besso" && n.value == "Andrea") {
                 document.getElementById(id).style.display = 'block';
+                document.querySelector("#divlog").style.display = "none";
+
             }
-        } else {
-            document.getElementById(id).style.display = 'none';
         }
     }
 }
+function cambiaColore(color) {
+    document.body.style.background = color;
+}
+function convertDate(inputFormat) {
+  function pad(s) { return (s < 10) ? '0' + s : s; }
+  var d = new Date(inputFormat)
+  return [pad(d.getDate()), pad(d.getMonth()+1), d.getFullYear()].join('/')+" "+[pad(d.getHours()), pad(d.getMinutes())].join(':')
+}
+
